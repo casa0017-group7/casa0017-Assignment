4df8a3c807857f54f85f5b1e64469a2a1e87cdf0
YikunLi9 <yikun.li19@gmail.com>
2023-11-07 18:27:09 +0000

    added route methods, some statements were changed to make the code more standardized

--- server/routes/resources.js
+++ server/routes/resources.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'CarbonMap'});
+    res.render('resources', {title: 'CarbonMap - Resources'});
 });
 
 module.exports = router;

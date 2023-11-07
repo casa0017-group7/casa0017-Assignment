749e8de7a61a1d58314884cb448cc162937ddc5b
zczqxc5 <zczqxc5@ucl.ac.uk>
2023-11-07 20:58:14 +0000

    Delete json.js

--- server/public/javascripts/json.js
+++
@@ -1,8 +0,0 @@
-fetch('http://localhost:3000/resources/UK.geojson')
-    .then(response => response.json())
-    .then(data => {
-      data.features.forEach(feature => {
-        // 访问属性并将其打印到控制台
-        console.log('name:', feature.properties.name);
-      });
-    });

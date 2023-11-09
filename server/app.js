88e24e934212f7d4c8f519840f5b771c2ffa23df
YikunLi9 <yikun.li19@gmail.com>
2023-11-09 16:59:44 +0000

    404 auto direct added

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var mapRouter = require('./routes/map');
 var resourcesRouter = require('./routes/resources');
 var aboutRouter = require('./routes/about');
+var errorRouter = require('./routes/error')
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use('/index', indexRouter);
 app.use('/map', mapRouter);
 app.use('/resources', resourcesRouter);
 app.use('/about', aboutRouter);
+app.use('*', errorRouter);
 
 
 

80617ff552ab56e79ba057e7dbd2d9d37603b704
zczqxc5 <zczqxc5@ucl.ac.uk>
2023-11-13 14:00:59 +0000

    add empty chart

--- server/public/javascripts/barchart.js
+++ server/public/javascripts/barchart.js
@@ -2,44 +2,36 @@ $(document).ready(function () {
   var id, shortname, formattedDate, formattedTime;
   var dataArray;
 
-  $("#dropdown").change(function(){
+  $("#dropdown").change(function () {
     id = $(this).val();
     shortname = $("#dropdown :selected").text();
   });
   $('#datetimepicker').datetimepicker({
-
-    defaultDate:0,
-    step:30,
-    minDate:'2020/01/01',
-    maxDate:0,
-    maxTime:0  
-}).change(function(){
+    defaultDate: 0,
+    step: 30,
+    minDate: '2020/01/01',
+    maxDate: 0,
+    maxTime: 0
+  }).change(function () {
     var startDate = $(this).val();
     var dateObject = new Date(startDate);
     formattedDate = dateObject.toISOString().split('T')[0];
-    formattedTime= dateObject.toLocaleTimeString('en-US', { hour12: false });
-
-    // console.log('Formatted Date:', formattedDate);
-    // console.log('Formatted Time:', formattedTime);
+    formattedTime = dateObject.toLocaleTimeString('en-US', { hour12: false });
   });
   $("#submit").click(function () {
-    //console.log('ID:', id, 'Shortname:', shortname, 'date:', formattedDate, 'time:', formattedTime);
     const requestData = {
-          regionid: id,
-          shortname: shortname,
-          date: formattedDate,
-          time: formattedTime
+      regionid: id,
+      shortname: shortname,
+      date: formattedDate,
+      time: formattedTime
     };
     const jsonData = JSON.stringify(requestData);
-    //console.log(requestData);
-  
     const url = "http://localhost:3000/map/data";
 
-    // Make the POST request using fetch
     fetch(url, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json', 
+        'Content-Type': 'application/json',
       },
       body: jsonData,
     })
@@ -50,49 +42,67 @@ $(document).ready(function () {
         return response.json();
       })
       .then(data => {
-        // Handle the response data
-        //console.log(data);
-      dataArray = JSON.parse(data[0].data);  
-      //console.log(dataArray);
-      drawBarChart(dataArray);
+        dataArray = JSON.parse(data[0].data);
+        drawBarChart(dataArray);
       })
       .catch(error => {
-        // Handle errors during the request
         console.error('Error:', error);
-
+      });
   });
 });
-})
-google.charts.load('current', {'packages':['bar']});
+
+google.charts.load('current', { 'packages': ['bar'] });
 google.charts.setOnLoadCallback(drawBarChart);
 
 function drawBarChart(data) {
-  var chartData = data;
-  var dataTable = new google.visualization.DataTable(chartData);
-  dataTable.addColumn('string', 'Fuel');
-  dataTable.addColumn('number', 'Percent');
+  // 检查数据是否为空
+  if (!data || data.length === 0) {
+    // 如果数据为空，创建一个空的数据表
+    var emptyDataTable = new google.visualization.DataTable();
+    emptyDataTable.addColumn('string', 'Fuel');
+    emptyDataTable.addColumn('number', 'Percent');
+    emptyDataTable.addRow(['', 0]); // 添加一个初始的空数据行
 
-  for (var i = 0; i < chartData.length; i++) {
-    dataTable.addRow([chartData[i].fuel, chartData[i].perc]);
-  }
-  var options = {
-    title: 'Fuel Percentage',
-    chartArea: { width: '50%' },
-    hAxis: {
-      title: 'Percentage',
-      minValue: 0,
-      maxValue: 100
-    },
-    vAxis: {
-      title: 'Fuel'
+    // 创建一个空白的图表
+    var options = {
+      title: 'Fuel Percentage',
+      chartArea: { width: '50%' },
+      hAxis: {
+        title: 'Percentage',
+        minValue: 0,
+        maxValue: 100
+      },
+      vAxis: {
+        title: 'Fuel'
+      }
+    };
+
+    var emptyChart = new google.charts.Bar(document.getElementById('barchart_material'));
+    emptyChart.draw(emptyDataTable, google.charts.Bar.convertOptions(options));
+  } else {
+    // 如果数据不为空，绘制正常的图表
+    var dataTable = new google.visualization.DataTable(data);
+    dataTable.addColumn('string', 'Fuel');
+    dataTable.addColumn('number', 'Percent');
+
+    for (var i = 0; i < data.length; i++) {
+      dataTable.addRow([data[i].fuel, data[i].perc]);
     }
-  };
 
-  var chart = new google.charts.Bar(document.getElementById('barchart_material'));
-  chart.draw(dataTable, google.charts.Bar.convertOptions(options));
-  //console.log(chart);
+    var options = {
+      title: 'Fuel Percentage',
+      chartArea: { width: '50%' },
+      hAxis: {
+        title: 'Percentage',
+        minValue: 0,
+        maxValue: 100
+      },
+      vAxis: {
+        title: 'Fuel'
+      }
+    };
+
+    var chart = new google.charts.Bar(document.getElementById('barchart_material'));
+    chart.draw(dataTable, google.charts.Bar.convertOptions(options));
+  }
 }
-
-
-
-
